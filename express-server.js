33e6770044a63c2f6e88b518ea0903224d29258d
Connor Roberts <connoobets@gmail.com>
2021-11-08 18:41:27 -0700

    created a form for input of long URLS and a function that generates a random 6 digit hexadecimal string and also installed body parser through npm and therefore changed .json files

--- express-server.js
+++ express-server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require('body-parser');
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
+const generateRandomString = function() {
+  const shortURL = Math.random().toString(16).substr(2, 6);
+  return shortURL;
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello!');
+  console.log(generateRandomString());
 });
 
 app.get('/urls', (req, res) => {
@@ -17,11 +24,21 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(templateVars);
+  // console.log(templateVars);
   res.render('urls_show', templateVars);
 });
+
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
