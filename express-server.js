e60597a55e9f31daabea6097a7d78f3ffd54bb31
Connor Roberts <connoobets@gmail.com>
2021-11-08 17:50:14 -0700

    implemented a route for shortURLS and made a urls_show.ejs file

--- express-server.js
+++ express-server.js
@@ -15,7 +15,13 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase, };
   res.render('urls_index', templateVars);
-})
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(templateVars);
+  res.render('urls_show', templateVars);
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
