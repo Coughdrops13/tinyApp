9c3b274055acf173ce6e487d6a7a5b818bebbb3b
Connor Roberts <connoobets@gmail.com>
2021-11-09 16:43:40 -0700

    waited too long to commit... added cookie-parser, added routes for logging in as a user and logging out, stored and erased cookies based on username input, added forms in the header for these

--- express-server.js
+++ express-server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
@@ -12,14 +13,26 @@ const generateRandomString = function() {
 };
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, };
+  // console.log(req.cookies);
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'], };
   res.render('urls_index', templateVars);
 });
 
@@ -32,11 +45,12 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies['username'], };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username'] };
   // console.log(templateVars);
   res.render('urls_show', templateVars);
 });
